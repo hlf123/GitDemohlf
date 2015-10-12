16b40e3fff9925d697241e45eb7ef75cb0b24a82
hlf123 <1070257802@qq.com>
2015-10-12 11:45:33 +0800

    object

---
+++ GitDemo/GitDemo/help.h
@@ -0,0 +1,13 @@
+//
+//  help.h
+//  GitDemo
+//
+//  Created by qianfeng007 on 15/10/12.
+//  Copyright (c) 2015年 honglongfei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface help : NSObject
+
+@end
---
+++ GitDemo/GitDemo/help.m
@@ -0,0 +1,13 @@
+//
+//  help.m
+//  GitDemo
+//
+//  Created by qianfeng007 on 15/10/12.
+//  Copyright (c) 2015年 honglongfei. All rights reserved.
+//
+
+#import "help.h"
+
+@implementation help
+
+@end
